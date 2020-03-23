b7e4a1c820e1ea64e617df643ac7218d58acaba8
Ruxin <ruxin@couree.com>
2020-03-23 19:52:12 -0400

    add logo and input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import bsg_logo from './bsg_logo.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="Logo">
+        <img src={bsg_logo} style={{height:50, marginTop:5, marginRight: 1200}}/>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Lucy.
+          Please upload your video
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form id="upload-form" action="upload.php" method="post" enctype="multipart/form-data" >
+  　　　   <input type="file" id="upload" name="upload" align="middle"/> <br/>
+  　　　   <input type="submit" value="Upload" align="middle"/>
+        </form>
       </header>
     </div>
   );
